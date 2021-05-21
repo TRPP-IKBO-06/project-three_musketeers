dafc273432d7b798aa816c5cf42106bbb973026e
Бабышев Максим <cmdazaza@gmail.com>
2021-05-21 22:06:13 +0300

    + Support for java secrets into CI

--- app/decrypt.sh
+++ app/decrypt.sh
@@ -2,3 +2,6 @@
 cd app
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output "google-services.json" "google-services.json.gpg"
+cd src/main/java/main/stager
+gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
+--output "SECRETS.java" "SECRETS.java.gpg"
